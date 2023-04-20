f6fcd275f2d051da51d9a61f49933ace69e0be0a
th-zen <mrtacocat3000@gmail.com>
2023-04-20 10:41:47 +0200

    fix: fix the folling requests
    Bei allen Buttons die anderen Schrift, die clean
    //	Wenn man über das Logo hovert, das Logo Weiß, Button Rahmen
        weiß, text weiß, und button background transparent
    //	Weniger Rundung beim selected work
    //	Zwischen Projektbild und team mehr abstand
    //	Zwischen team und members mehr abstand
    //	Zwischen members und Kontakt mehr abstand
    //	Member fotos mit Rundung
    //	Bei members auch die clean Schrift nehmen
    //	Bei der Projects Page die Farben ändern (weiß blau)
    //	Scroll lock bei mobile ausschalten
    //	EXPLORE BUTTON ENTFERNEN
    //	Bei team members ganz rechts noch Jobtitel
        Contact ganz runter und Contact Page entfernen
    //  CLAIM Text kleiner
    //	Impressum in Contact Page weg(?)

--- javascript.js
+++ javascript.js
@@ -1,19 +1,19 @@
 // Disable scrolling on load
-window.addEventListener("load", function () {
-  document.body.style.overflow = "hidden";
-});
+// window.addEventListener("load", function () {
+//   document.body.style.overflow = "hidden";
+// });
 
 // Smooth scroll to content on button click
-var scrollButton = document.getElementById("explore_btn");
-scrollButton.addEventListener("click", function () {
-  var content = document.getElementById("statement_container");
-  content.scrollIntoView({ behavior: "smooth" });
+// var scrollButton = document.getElementById("explore_btn");
+// scrollButton.addEventListener("click", function () {
+//   var content = document.getElementById("statement_container");
+//   content.scrollIntoView({ behavior: "smooth" });
 
   // Enable scrolling after scrolling is complete
-  setTimeout(function () {
-    document.body.style.overflow = "";
-  }, 1000); // Change this delay to match the duration of your scroll animation
-});
+//   setTimeout(function () {
+//     document.body.style.overflow = "";
+//   }, 1000); // Change this delay to match the duration of your scroll animation
+// });
 
 //play the landing page highlightreel when hovering over the logo
 const logo = document.getElementById("landing_logo");
@@ -44,3 +44,30 @@ for (var i=0; i<checkboxes.length; i++) {
         }
     });
 }
+
+// Select the image and all navigation links
+const landingLogo = document.getElementById("landing_logo");
+const navLinks = document.querySelectorAll(".navbar a");
+
+// Add an event listener to the image for when it's hovered over
+landingLogo.addEventListener("mouseover", function() {
+  // Loop through all the navigation links
+  for(let i = 0; i < navLinks.length; i++) {
+    // Change the border, background, and color
+    navLinks[i].style.border = "solid 1px #ffffff";
+    navLinks[i].style.background = "transparent";
+    navLinks[i].style.color = "#ffffff";
+  }
+});
+
+// Add an event listener to the image for when it's not being hovered over
+landingLogo.addEventListener("mouseout", function() {
+  // Loop through all the navigation links
+  for(let i = 0; i < navLinks.length; i++) {
+    // Change the border, background, and color back to their original values
+    navLinks[i].style.border = "solid 1px #005CA9";
+    navLinks[i].style.background = "#ffffff";
+    navLinks[i].style.color = "inherit";
+  }
+});
+
